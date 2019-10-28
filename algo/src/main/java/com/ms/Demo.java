a858d3e4e380ef372b7630c293f4dd9b3cf5b78a
wangjian <805539693@qq.com>
2019-10-28 12:33:09 +0800

    学习算法16

--- algo/src/main/java/com/ms/Demo.java
+++ algo/src/main/java/com/ms/Demo.java
@@ -1,12 +1,39 @@
 package com.ms;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Demo {
+
     public static void main(String[] args) {
-        String str="111 222 333 444 555";
-        int i = str.lastIndexOf(" ");
-        System.out.println(i);
-        System.out.println(str.substring(i).length());
+        Scanner s = new Scanner(System.in);
+        while (s.hasNext()) {
+            String str = s.nextLine().trim();
+            StringBuilder builder = new StringBuilder();
+            for (int j = 0; j < 26; j++) {
+                for (int i = 0; i < str.length(); i++) {
+                    char c = str.charAt(i);
+                    if (c == (j + 'a') || c == (j + 'A')) {
+                        builder.append(c);
+                    }
+                }
+            }
+            int k = 1;
+            StringBuilder bu = new StringBuilder();
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                if (!isLetter(ch)) {
+                    bu.append(ch);
+                } else {
+                    bu.append(builder.charAt(k));
+                    k++;
+                }
+            }
+            System.out.println(bu.toString());
+        }
     }
+
+    private static boolean isLetter(char c) {
+        return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+    }
+
 }
